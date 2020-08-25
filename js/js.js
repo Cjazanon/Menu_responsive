c4a5335ddbef974ef9c8288edf5fb1ce377b1f9e
Cjazanon <azanonc@gmail.com>
2020-08-25 13:12:13 -0600

    actualizacion 3

--- js/js.js
+++ js/js.js
@@ -1,10 +1,14 @@
 var botonlis4 = document.getElementById('lisboton4');
 var boton4 = document.getElementById('boton4');
 
-
-function desplegarboton4() {
+function desplegarBoton4() {
   botonlis4.classList.add('mostrar');
 
 };
+function ocultarBoton4() {
+  botonlis4.classList.remove('mostrar');
 
-boton4.addEventListener("click", desplegarboton4,true);
+};
+
+boton4.addEventListener("mouseover", desplegarBoton4,true);
+boton4.addEventListener("mouseout", ocultarBoton4,true);
